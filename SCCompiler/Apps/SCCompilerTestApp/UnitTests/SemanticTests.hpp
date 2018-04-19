ebc1178df975307a592dea852a1bb981212d67b4
Arkin Terli <arkin.terli@gmail.com>
2018-04-18 17:38:18 -0700

    Missed this change to commit.

---
+++ SCCompiler/Apps/SCCompilerTestApp/UnitTests/SemanticTests.hpp
@@ -0,0 +1,40 @@
+//
+//  SemanticTests.hpp
+//  SCCompiler
+//
+//  Created by Arkin Terli on 4/18/18.
+//  Copyright © 2018 Arkin Terli. All rights reserved.
+//
+
+#pragma once
+
+#include <string>
+#include <stdexcept>
+#include <cppunit/extensions/HelperMacros.h>
+#include "Compiler.hpp"
+
+class SemanticTests : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE( SemanticTests );
+    
+    CPPUNIT_TEST( SemanticUndeclaredIdentifierTest );
+    
+    CPPUNIT_TEST_SUITE_END();
+    
+public:
+    
+    // Common test setup and cleanup.
+    void setUp();
+    void tearDown();
+    
+    // Tests given source code.
+    void TestCode(std::string sourceCode,
+                  SCCompiler::CompileResult expectedCompilerResult,
+                  bool acceptExpectedCompilerResult);
+    
+    // Semantic Tests
+    
+    void SemanticUndeclaredIdentifierTest();
+};
+
+
