80ef67999556f4ac46d169934f25ea553b55dd24
Arkin Terli <arkin.terli@gmail.com>
2018-04-22 16:18:37 -0700

    Add LLVM 6.0.0 source code.

---
+++ Externals/LLVM/6.0.0/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# The following line requires for macOS, otherwise configure application fails to find grep.
+export GREP_OPTIONS="--color=auto"
+
+rm -rf $PWD/build
+make clean
+./configure --prefix=$PWD/build --disable-shared --enable-static
+make -j 8
+
+make install
+
+# Clean intermediate build files.
+make clean
