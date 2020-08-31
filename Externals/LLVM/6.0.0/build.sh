3ac765607df00c14eca23951eee39938880a0876
Arkin <arkin.terli@gmail.com>
2020-08-30 20:59:55 -0700

    Update LLVM build.sh file.

--- Externals/LLVM/6.0.0/build.sh
+++ Externals/LLVM/6.0.0/build.sh
@@ -3,6 +3,7 @@
 # The following line requires for macOS, otherwise configure application fails to find grep.
 export GREP_OPTIONS="--color=auto"
 
+pushd .
 rm -rf build
 mkdir build
 cd build
@@ -10,3 +11,7 @@ cd build
 # Supported build types Debug, Release, RelWithDebInfo, and MinSizeRel
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../installed
 cmake --build . --target install -- -j 8
+
+popd
+
+rm -rf build
