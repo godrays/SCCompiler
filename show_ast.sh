91d867e0757bcbbf5e4d025d7f32d92baba78930
Arkin <arkin.terli@gmail.com>
2024-05-01 22:08:41 -0700

    Update show_dot.sh file for better user experience. Remove show_ast.sh file.

--- show_ast.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Make sure graphviz is installed and dot command line application is in the path.
-dot -Tsvg -o ast.svg ast.dot && open ast.svg
--- show_dot.sh
+++ show_dot.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-# Make sure graphviz is installed and dot command line application is in the path.
-dot -Tsvg -o ast.svg $1 && open ast.svg
+function showHelp()
+{
+    echo "Shows/Visualizes GraphViz dot file."
+    echo ""
+    echo "Usage:"
+    echo "    $0 <filename>"
+    echo ""
+    echo "Example:"
+    echo "    $0 ast.dot"
+    echo ""
+}
+
+function main()
+{
+    if command -v dot >/dev/null 2>&1; then
+        dot -Tsvg -o ast.svg $1 && open ast.svg
+    else
+        echo "Graphviz 'dot' command is not installed."
+    fi
+}
+
+if [ "$#" -ge 1 ]; then
+    main "$@"  # Pass all parameters
+else
+    showHelp
+fi
